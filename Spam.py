5e77d752421fd968d7c1e50959ca23e2f19823fe
MR.L4M3R <83567167+MR-L4M3R@users.noreply.github.com>
2021-06-05 21:30:44 +0700

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -691,7 +691,7 @@ try:
 				sys.exit()
 		
 		elif PilihTools == '9':
-			os.system('xdg-open http://wa.me/6285880818385?text=Hallo')
+			os.system('xdg-open http://wa.me/+1(507)5790039?text=Hallo')
 		
 		elif PilihTools == 'X' or PilihTools == 'x':
 			print
